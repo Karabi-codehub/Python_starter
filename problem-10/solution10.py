02d312d8994ed3c2738e8a13fa6b02f3e05e2a58
Karabi kumari Medha <u1604062@student.cuet.ac.bd>
2025-05-21 21:08:03 +0600

    Delete problem-10 directory

--- problem-10/solution10.py
+++
@@ -1,7 +0,0 @@
-data_list = [13, 24, 'Karim', {'name': 'guru'}, 45, 17]
-
-# Create a new list with only integer values
-cleaned_list=[item for item in data_list if type(item)==int]
-
-print("data_list",data_list)
-print("cleaned_list",cleaned_list)
